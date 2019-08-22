34945f9127a40ef3c4d898812da2fe5b1e18f96a
Rose Marshack <rose@posterchildren.com>
2019-08-22 11:11:02 -0500

    because

--- p5/circle/sketch.js
+++ p5/circle/sketch.js
@@ -5,6 +5,10 @@ function setup() {
 
 function draw() {
   // put drawing code here
+  rect(mouseX, mouseY, 10, 10);
 
-  ellipse(mouseX, mouseY, 10, 10) ;
 }
+
+// function mouseDragged() {
+//   ellipse(mouseX, mouseY, 10, 10) ;
+// }

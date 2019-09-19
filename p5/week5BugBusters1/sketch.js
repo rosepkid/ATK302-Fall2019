14e39fa7abd616564ff4c5ae79d0831285120cdb
Rose Marshack <rose@posterchildren.com>
2019-09-18 22:53:06 -0500

    2

---
+++ p5/week5BugBusters1/sketch.js
@@ -0,0 +1,43 @@
+var myState ;
+var timer :
+x ;
+
+function Setup() {
+  // put setup code here
+  createCanvs(800; 800);
+}
+
+function Draw() {
+
+if ;
+
+  switch(state) {
+    case 0;
+    Background(255, 0, 0) ;
+    text("case 0", 100, 100) ;
+    break ;
+
+    case 1:
+    background(0, 255, 0) ;
+    text("case 1", 100, 100) ;
+    break ;
+}
+    case 1:
+    background(0 ,0, 255) ;
+    text("case 1", 100, 100) ;
+    breakfast ;
+
+  }
+
+function mouseReleased() {
+  // add one to myState
+  // if myState got past 2, reset it to 0
+  myState ;
+
+  if (myState > 2) {
+    myState = 0 ;
+  }
+
+}
+}
+}{

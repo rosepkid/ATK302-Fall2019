b079b512426039ff310c48bff2e2104ff69a60e8
Rose Marshack <rose@posterchildren.com>
2019-10-28 20:57:33 -0500

    3

--- p5/Week11/geo/sketch.js
+++ p5/Week11/geo/sketch.js
@@ -5,14 +5,14 @@ function preload(){
 }
 
 function setup() {
-	createCanvas(500, 500) ; 
-	
+	createCanvas(500, 500) ;
+
 }
 
 function draw() {
-	fill(0) ; 
-	textSize(12) ; 
-	text("hello", 20, 20) ; 
+	fill(0) ;
+	textSize(12) ;
+	text("hello", 20, 20) ;
     text(locationData.latitude, 40, 40) ;
     text(locationData.longitude, 40, 60) ;
     text(locationData.accuracy, 40, 80) ;
@@ -20,5 +20,5 @@ function draw() {
     text(locationData.altitudeAccuracy, 40, 120) ;
     text(locationData.heading, 40, 140) ;
     text(locationData.speed, 40, 160) ;
-	
+
 }

09627306f2f048f2afc7f82dfe45b4bf5aaf2392
Rose Marshack <rose@posterchildren.com>
2019-11-19 22:32:43 -0600

    1

---
+++ p5/ChabelaAndJames/sketch.js
@@ -0,0 +1,258 @@
+var myState = 0;
+var myScore = 0;
+var numQuestionsAsked = 0;
+var myLogo;
+var capriola;
+var corben;
+
+// THIS ONE POINTS TO YOURS!!!
+//var myQuiz = '1tTYEvW3bssQF9XLH88e7n7VrTGK4mwPCPnAyCZRuvFM';
+
+// THIS ONE POINTS TO MY TEST ONE!!
+var myQuiz = '1THGjGo17uHS2MW6IHAXFD602sRWrEWS0_U5khU9TWAM';
+
+
+var questionsArray = [];
+var rq = 0;
+var timer = 0;
+var randomAnswer = 0;
+var correctLineTop = 175; // this is the top-most of where the correct answer is and it gets reset in the question display
+
+function preload() {
+  myLogo = loadImage("assets/thetquizlogo-02.png");
+  capriola = loadFont("assets/Capriola.ttf");
+  corben = loadFont("assets/Corben.ttf");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  unloadScrollBars();
+  imageMode(CENTER);
+  textAlign(CENTER);
+  rectMode(CENTER);
+
+  let settings = {
+    key: myQuiz, // The url of the published google sheet
+    callback: gotData, // A callback for when the data comes in
+    simpleSheet: true // This makes things simpler for just a single worksheet of rows
+  };
+
+  Tabletop.init(settings);
+
+}
+
+function gotData(data) {
+
+  console.log(data); // Print the data in the console
+
+  // iterate through the array of data and create an object and push it on an array called namesArray
+  for (let i = 0; i < data.length; i++) {
+    questionsArray.push(new Question(data[i].Question, data[i].A, data[i].B, data[i].C, data[i].D, data[i].Answer));
+  }
+
+}
+
+function draw() {
+  noStroke();
+
+  switch (myState) {
+    case 0:
+      background('#455160'); //gray
+      image(myLogo, width / 2, height / 2 - 50, 152.2 * 3, 118.8 * 3);
+      fill('#d8d8d8'); //white
+      textSize(16);
+      textFont(capriola);
+      text("Click \"START\" to play", width / 2, height / 2 + 190);
+      fill('#e1cb28'); //yellow
+      rect(width / 2, height / 2 + 240, 120, 50, 10);
+      fill('#455160'); //gray
+      textSize(28);
+      text('START', width / 2, height / 2 + 250);
+      break;
+
+    case 1:
+      // get a random question out
+      background('#455160'); //gray
+      textAlign(LEFT);
+      image(myLogo, width / 12, height / 7, 152.2, 118.8)
+
+      fill('#e1cb28');
+      rect(width / 2, height - 50, windowWidth, 100); //yellow bar
+      fill('black');
+      textFont(corben);
+      textSize(60);
+      text('Score: 1000', 190, height - 15); //Score
+      fill('#455160');
+      rect(width / 2 + 100, height - 50, 400, 75); //Scoreboard
+      fill('black');
+      textSize(100);
+      textFont(corben);
+      text('T', width - 100, height - 15); //Progress T
+      textSize(12);
+      text(myScore + "/" + numQuestionsAsked, width - 150, height - 15); //Question progress
+
+      var m = floor(random(questionsArray.length));
+      rq = questionsArray[m];
+
+      if (rq != null) {  // once in a while rq ends up null, not sure why.
+        rq.display();
+
+        myState = 2;
+      } else {
+        console.log("received a null rq");
+        myState = 1;
+      }
+      break;
+
+    case 2:
+
+      break;
+
+    case 20:
+      background('green');
+      text("welcome to case 20 cool fun experience", 100, 100);
+      break;
+
+
+    case 30:
+      background('blue');
+      text("welcome to case 30 awesome hip type fun", 100, 100);
+      break;
+
+
+    case 100:
+      // go to a random state!
+
+      // if numQuestionsAsked == 10, go to state 0 or some "win" state?
+      if (numQuestionsAsked == 10) {
+        myState = 101;
+      } else {
+        // what state to go to?
+        // most of the time, let's go to state 1.
+        let whatState = random(100);
+        if (whatState > 10) {
+          myState = 1;
+        } else {
+          if (whatState < 5) {
+            myState = 20;
+          } else {
+            myState = 30;
+          }
+        }
+      }
+
+      break;
+
+    case 101:
+      background('grey');
+      text("Thanks for playing!\nScore: " + myScore + "\\" + numQuestionsAsked, width / 2, height / 2);
+      break;
+  }
+
+
+
+}
+
+
+
+function mouseReleased() {
+  switch (myState) {
+
+    case 0: // the splash screen
+      myState = 100; // 100 is the state that goes to a random thingy
+      break;
+
+
+    case 2: // the random question pulled from database
+      if ((mouseX > 740) && (mouseY > correctLineTop) && (mouseY < correctLineTop + 50)) {
+        myScore++;
+        console.log('yay');
+      } else {
+        console.log('nope!');
+      }
+      numQuestionsAsked++; // maybe this only gets done if mouseX > 740?
+
+      myState = 100; // 100 is the state that goes to a random thingy
+      break;
+
+    case 20:
+      myState = 100;
+      break;
+
+    case 30:
+      myState = 100;
+      break;
+
+    case 101:
+      myState = 0;
+      // this is a good place to reset everything if you need to
+      numQuestionsAsked = 0;
+      myScore = 0;
+      break;
+
+  }
+
+  console.log(mouseX + ',' + mouseY);
+}
+
+function unloadScrollBars() {
+  document.documentElement.style.overflow = 'hidden'; // firefox, chrome
+  document.body.scroll = "no"; // ie only
+}
+
+
+// The QUESTION Class!!!
+function Question(myQuestion, myA, myB, myC, myD, myAnswer) {
+
+  // attributes
+  this.pos = createVector(random(width), random(height));
+  this.question = myQuestion;
+  this.a = myA;
+  this.b = myB;
+  this.c = myC;
+  this.d = myD;
+  this.answer = myAnswer; // we aren't using this.
+
+
+  // methods
+  this.display = function() {
+
+    // display the question
+    fill('#e1cb28');
+    textSize(32);
+    text(this.question, 750, 150);
+
+    // display the answers
+    textFont(capriola);
+    textSize(16);
+    fill('#d8d8d8');
+
+    // push all the answers onto a temporary array
+    var ary = [];
+    ary.push(this.a);
+    ary.push(this.b);
+    ary.push(this.c);
+    ary.push(this.d);
+
+    // go through the temporary array 4 times and "pop" and display a random answer
+    for (var i = 0; i < 4; i++) {
+
+      // get a random answer out
+      var r = floor(random(ary.length)); // choose a random answer to take out
+
+      randomAnswer = ary[r]; // here's the random answer (a String)
+      //  console.log("randomAnswer = " + randomAnswer) ;
+      ary.splice(r, 1); // pop that element out of the array so we don't re-use it
+
+      if (randomAnswer.charAt(0) == "*") { // is it the correct answer?
+        randomAnswer = randomAnswer.substr(1); // delete that star off (char 0) if it is!
+        correctLineTop = 175 + (i * 50); // set the correct line top depending on i
+      }
+
+      text(randomAnswer, 750, 200 + i * 50); // display that answer
+
+    }
+
+  }
+
+}
--- p5/geoWelcomeWeek/sketch.js
+++ p5/geoWelcomeWeek/sketch.js
@@ -1,70 +1,92 @@
-// GEOLOCATION DATA
-// This doesn't seem to work.
+// Spring 2019 Curtis Stieger
 
+var fence;
 var locationData;
+var num;
+var distance;
+var places = [];
+var reggieImg;
 
-function preload(){
-   locationData =  getCurrentPosition();
-  // getCurrentPosition(doThisOnLocation, oops) ;
+function preload() {
+  locationData = getCurrentPosition();
+  reggieImg = loadImage('assets/reggiePlaceholder.jpeg');
+  // TURN "reggieImg;" into "reggieImg = [];" to create an array of different images
+  //reggieImg = loadImage('assets/reggiePlaceholder.jpeg'); //these will change to images of pics in front of the buildings for welcome week
+  //reggieImg[2] = loadImage('assets/reggiePlaceholder.jpeg');
 }
 
 function setup() {
-	createCanvas(displayWidth, displayHeight) ;
-	textSize(24) ;
-    text(("lat: "+locationData.latitude), 10, 40) ;
-	 text(("lon: "+locationData.longitude), 10, 80) ;
- //   text(("lon: "+locationData.longitude).toFixed(6), 10, 40) ;
-    text("accuracy = " + locationData.accuracy, 10, 120) ;
-/*
-    text(locationData.altitude, 10, 100) ;
-    text(locationData.altitudeAccuracy, 10, 120);
-    text(locationData.heading, 10, 150);
-    text(locationData.speed), 10, 180;
-	*/
-	watchPosition(positionChanged) ;
+  //fence = new geoFenceCircle(40.506229, -88.990537, 0.02); //sets geofence location to cva room 17
+  createCanvas(displayWidth, displayHeight);
+  num = 0;
+  intervalCurrentPosition(positionPing, 5000);
+  places.push(new Place(40.50622797365503, -88.99051350503431, "CVA 17", .02, reggieImg)); // new Place object, for CVA room 17
+  places.push(new Place(40.50715473783438, -88.99173550368103, "COB", .02, reggieImg)); // new Place object, for COB.... JUST SWITCHED TO NEW COORDINATES
+  places.push(new Place(40.510824736433904, -88.99134151266699, "ISU College Bridge", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
 
+  places.push(new Place(40.50863221414712, -88.99077591254148, "Old Union", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50840289459472, -88.9909118880512, "Williams Hall", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50844449497366, -88.9911676488728, "Cent 4 Perf Arts", .02), reggieImg); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50750741811689, -88.99029850463533, "CVA Circle", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.5079598008354, -88.99148671475066, "McCormick Hall", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50864821960959, -88.99120123764614, "Fell Hall", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50896516728555, -88.99212919431163, "DeGarmo Hall Solar", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50917235949953, -88.99177097641105, "Cook Hall", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50947612369081, -88.99174125561485, "Edwards Hall", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50980921328644, -88.99149564020013, "Schroeder Hall", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.51014656358694, -88.9912748009074, "Felmley Hall", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50942119788225, -88.99076697407767, "Moulton Hall", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50930542714758, -88.99068753755874, "Hovey Hall", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.508730456808415, -88.98572041960726, "University Galleries", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.511676506705506, -88.9938474159579, "Student Services Building", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.51039740494815, -88.9996815241351, "Nelson Smith", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.512969573012896, -88.99488587696477, "Hancock Stadium", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.509699669963155, -88.99664232253424, "Turner Hall", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.512242304908746, -88.99975734818341, "Tri Towers (Haynie)", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50946288329222, -88.98459824742137, "Uptown Circle", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.47137301266825, -88.94350239220492, "Check Location", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
 
-	 fence = new geoFenceCircle(locationData.latitude, locationData.longitude, .05, insideTheFence, outsideTheFence, 'mi');
 }
 
 function draw() {
-	background(255) ;
-	  text(("lat: "+locationData.latitude), 10, 40) ;
-	 text(("lon: "+locationData.longitude), 10, 80) ;
- //   text(("lon: "+locationData.longitude).toFixed(6), 10, 40) ;
-    text("accuracy = " + locationData.accuracy, 10, 120) ;
 
-	 if (fence.insideFence==true) {
-		 text("inside fence", 10, 220); }
-	 else {
-		 text("outside fence", 10, 220) ;
-	 }
-
-}
-function oops() {
-	println("oops") ;
+  // for (var i = 0, i < places.length, i++) {
+  //   print(places[i].fence.insideFence);
+  // }
 }
 
+function positionPing(position) {
+  textSize(24);
+  num++;
+  background(255);
+  text("lat: " + position.latitude.toFixed(8), 10, 340);
+  text("long: " + position.longitude.toFixed(8), 10, 390);
+  text("number of updates: " + num, 10, 440);
+  distance = calcGeoDistance(locationData.latitude, locationData.longitude, position.latitude, position.longitude, 'mi');
 
-function positionChanged(position){
-    text("Position changed! lat: " + position.latitude, 10, 250);
-    text("long: " + position.longitude, 10, 280);
+  for (var i = 0; i < places.length; i++) {
+    if (places[i].fence.insideFence === true){
+      places[i].display();
+      break; //should break out of the for loop?
+      //text(places[i].desc + ' check1 ' + places[i].fence.insideFence, 10, 240 + (i * 28));
+    }
+  }
+
 }
 
-function doThisOnLocation(position){
-	text("do this on location", 100, 40) ;
-    text("lat: " + position.latitude, 100, 80);
-    text("long: " + position.longitude, 100, 120);
-}
+function Place(lat, long, desc, radius, reggieImg) {
+  this.lat = lat;
+  this.long = long;
+  this.fence = false;
+  this.desc = desc;
+  this.radius = radius;
+  this.fence = new geoFenceCircle(this.lat, this.long, this.radius); //sets geofence location to coordinates
 
-function insideTheFence(position){
-    print("INlat: " + position.latitude);
-    print("INlong: " + position.longitude);
-    text("user is inside of the fence", 100, 220) ;
-}
 
-function outsideTheFence(position){
-    print("OUTlat: " + position.latitude);
-    print("OUTlong: " + position.longitude);
-    text("user is outside of the fence", 100, 220);
+
+  this.display = function() {
+    image(reggieImg, 10, 10);
+    textSize(20);
+    text("You are totally at " + this.desc, 10, 240);
+  }
 }

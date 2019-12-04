46878339f5a2493a1ffdffc138c1c427f5902586
Rose Marshack <rose@posterchildren.com>
2019-12-04 09:40:20 -0600

    Gyro blank with just info

---
+++ p5/gyro/sketch.js
@@ -0,0 +1,71 @@
+
+var alpha, beta, gamma; // orientation data
+var x = 0; // acceleration data
+var y = 0;
+var z = 0;
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+ // requestT() ;
+
+  // initialize accelerometer variables
+  alpha = 0;
+  beta = 0;
+  gamma = 0;
+
+  imageMode(CENTER);
+  rectMode(CENTER);
+
+}
+
+function draw() {
+
+  background('#c6f5ff'); // light blue
+
+  // the map command !!!!
+  // takes your variable and maps it from range 1 to range 2
+  // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
+  xPosition = map(gamma, -60, 60, 0, width);
+  yPosition = map(beta, -30, 30, 0, height);
+
+
+
+  // DECORATIONS
+  // Just a bunch of text commands to display data coming in from addEventListeners
+  textAlign(LEFT);
+  textSize(20);
+  fill('black');
+  text("orientation data:", 25, 25);
+  textSize(15);
+  text("alpha: " + alpha, 25, 50);
+  text("beta: " + beta, 25, 70);
+  text("gamma: " + gamma, 25, 90);
+  textSize(20);
+  text("acceleration data:", 25, 125);
+  textSize(15);
+
+
+  text("x = " + x, 25, 150); // .toFixed means just show (x) decimal places
+  text("y = " + y, 25, 170);
+  text("z = " + z, 25, 190);
+
+}
+
+// HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
+
+// Read in accelerometer data
+window.addEventListener('deviceorientation', function(e) {
+  alpha = e.alpha;
+  beta = e.beta;
+  gamma = e.gamma;
+});
+
+
+// accelerometer Data
+window.addEventListener('devicemotion', function(e) {
+  // get accelerometer values
+  x = e.acceleration.x;
+  y = e.acceleration.y;
+  z = e.acceleration.z;
+});

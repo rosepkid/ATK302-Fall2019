c990e517938120a160bce245a1d427e4bf71772b
Rose Marshack <rose@posterchildren.com>
2019-12-14 16:18:48 -0600

    graduated!

--- p5/chabelafinal/sketch.js
+++ p5/chabelafinal/sketch.js
@@ -8,6 +8,7 @@ var kering;
 var n;
 var slider;
 var imagesArray = [];
+var d ;
 
 // THIS ONE POINTS TO YOURS!!!
 var myQuiz = '1tTYEvW3bssQF9XLH88e7n7VrTGK4mwPCPnAyCZRuvFM';
@@ -54,23 +55,29 @@ function setup() {
 function gotData(data) {
 
   console.log(data); // Print the data in the console
+d = data ;
+  loadTheQuestions(d);
 
+
+}
+
+function loadTheQuestions(data) {
   // iterate through the array of data and create an object and push it on an array called namesArray
   for (let i = 1; i < data.length; i++) {
     questionsArray.push(new Question(i, data[i].Question, data[i].A, data[i].B, data[i].C, data[i].D, data[i].Answer));
   }
   for (var i = 0; i < data.length; i++) {
-    imagesArray[i] = loadImage("assets/"+i+".png");
+    imagesArray[i] = loadImage("assets/" + i + ".png");
   }
-
 }
+
 
 function draw() {
   noStroke();
 
   switch (myState) {
     case 0:
-    textAlign(CENTER);
+      textAlign(CENTER);
       background('#455160'); //gray
       image(myLogo, 600, 250, 152.2 * 3, 118.8 * 3);
       fill('#d8d8d8'); //white
@@ -95,7 +102,7 @@ function draw() {
       fill('black');
       textFont(corben);
       textSize(60);
-      text("Score: " + myScore*100, 50, 585); //Score
+      text("Score: " + myScore * 100, 50, 585); //Score
       fill('#455160');
       fill('black');
       textSize(100);
@@ -104,13 +111,13 @@ function draw() {
       text(numQuestionsAsked + " / 33", 1050, 585); //Question progress
 
       var m = floor(random(questionsArray.length));
-  //    m++ ;
-      console.log("doing question "+ m) ;
+      //    m++ ;
+      console.log("doing question " + m);
       rq = questionsArray[m];
 
       if (rq != null) { // once in a while rq ends up null, not sure why.
         rq.display();
-questionsArray.splice(m,1);
+        questionsArray.splice(m, 1);
         myState = 2;
       } else {
         console.log("received a null rq number" + m);
@@ -119,50 +126,50 @@ questionsArray.splice(m,1);
       break;
 
     case 2:
-    // in this state the question is displayed and we are just waiting for the user to answer it.
-    // we will exit this state from the mouseReleased function.
+      // in this state the question is displayed and we are just waiting for the user to answer it.
+      // we will exit this state from the mouseReleased function.
 
       break;
 
     case 20:
-    //background display
-    background('#455160'); //gray
-    textAlign(LEFT);
-    image(myLogo, 100, 85.7142857, 152.2, 118.8)
-    fill('#e1cb28');
-    rect(600, 550, 1400, 100); //yellow bar
-    fill('black');
-    textFont(corben);
-    textSize(60);
-    text('Score:' + myScore*100, 50, 585); //Score
-    fill('black');
-    textSize(12);
-    text(numQuestionsAsked + " / 33", 1050, 585); //Question progress
+      //background display
+      background('#455160'); //gray
+      textAlign(LEFT);
+      image(myLogo, 100, 85.7142857, 152.2, 118.8)
+      fill('#e1cb28');
+      rect(600, 550, 1400, 100); //yellow bar
+      fill('black');
+      textFont(corben);
+      textSize(60);
+      text('Score:' + myScore * 100, 50, 585); //Score
+      fill('black');
+      textSize(12);
+      text(numQuestionsAsked + " / 33", 1050, 585); //Question progress
 
-    // display the question
-    fill('#e1cb28');
-    textSize(32);
-    text('Kern the Word Correctly', 750, 150);
-
-
-    slider.position(390, 250);
-    image(kering, 450, 200, 761 / 3, 198 / 3);
-    image(n, slider.value()+400, 200, 761 / 3, 198 / 3);
-    slider.show();
+      // display the question
+      fill('#e1cb28');
+      textSize(32);
+      text('Kern the Word Correctly', 750, 150);
 
 
-    fill('#d8d8d8');
-    rect(455, 320, 75, 25, 10);
-    fill('#455160');
-    textAlign(CENTER);
-    textSize(18);
-    text('Done', 455, 325);
-    break;
+      slider.position(390, 250);
+      image(kering, 450, 200, 761 / 3, 198 / 3);
+      image(n, slider.value() + 400, 200, 761 / 3, 198 / 3);
+      slider.show();
 
-  case 21:
-    slider.hide();
-    myState = 100;
-    break;
+
+      fill('#d8d8d8');
+      rect(455, 320, 75, 25, 10);
+      fill('#455160');
+      textAlign(CENTER);
+      textSize(18);
+      text('Done', 455, 325);
+      break;
+
+    case 21:
+      slider.hide();
+      myState = 100;
+      break;
 
 
 
@@ -170,8 +177,8 @@ questionsArray.splice(m,1);
       // go to a random state!
 
       // if numQuestionsAsked == 10, go to state 0 or some "win" state?
-  //    if (numQuestionsAsked == 33) {
-  if (questionsArray.length() == 0)
+      //    if (numQuestionsAsked == 33) {
+      if (questionsArray.length == 0) {
         myState = 101; // go to the end state.
       } else {
         // what state to go to?
@@ -187,6 +194,7 @@ questionsArray.splice(m,1);
           }
         }
       }
+
 
       break;
 
@@ -223,29 +231,30 @@ function mouseReleased() {
       myState = 100; // 100 is the state that goes to a random thingy
       break;
 
-      case 20: //kerning
-          //myState = 100;
-          if ((mouseX > 418) && (mouseX < 494) && (mouseY > 308) && (mouseY < 334)) {
-            myState = 21;
-          }
+    case 20: //kerning
+      //myState = 100;
+      if ((mouseX > 418) && (mouseX < 494) && (mouseY > 308) && (mouseY < 334)) {
+        myState = 21;
+      }
 
-          if ((mouseX > 418) && (mouseX < 494) && (mouseY > 308) && (mouseY < 334) && (slider.value > 453) && (slider.value < 457)) {
-            myScore++;
-          }
+      if ((mouseX > 418) && (mouseX < 494) && (mouseY > 308) && (mouseY < 334) && (slider.value > 453) && (slider.value < 457)) {
+        myScore++;
+      }
 
       case 21:
-      break;
+        break;
 
-    case 30:
-      myState = 100;
-      break;
+      case 30:
+        myState = 100;
+        break;
 
-    case 101:
-      myState = 0;
-      // this is a good place to reset everything if you need to
-      numQuestionsAsked = 0;
-      myScore = 0;
-      break;
+      case 101:
+        myState = 0;
+        // this is a good place to reset everything if you need to
+        loadTheQuestions(d);
+        numQuestionsAsked = 0;
+        myScore = 0;
+        break;
 
   }
 
@@ -292,7 +301,7 @@ function Question(myNumber, myQuestion, myA, myB, myC, myD, myAnswer) {
 
     // display the image
     image(imagesArray[this.number], 450, 280, 400, 400);
-  //image(imagesArray[18], 450, 280, 400, 400);
+    //image(imagesArray[18], 450, 280, 400, 400);
 
     // display the answers
     textFont(capriola);
